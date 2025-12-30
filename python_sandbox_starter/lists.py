4c16a79ee0d22f15aff78b4d1ca7875876551a50
julietoge <julietogechi27@gmail.com>
2025-12-30 22:48:21 +0100

    learning python

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,12 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+
+#Create list (common way)
+numbers = [1, 2, 3, 4, 5]
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
+
+#Use a constructor to make a list
+fruits2 = list(('apple', 'banana', 'cherry'))
+
+print(numbers, fruits2)
+print(fruits[1])
--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,18 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
+name = 'jule'
+age = 26
 
+#concatenate
+print('Hello, my name is ' + name + ' and I am ' + str(age))
 
 # String Formatting
+print('My name is {name} and I am {age}'.format(name=name, age=age))
+
+#F-string
+print(f'My name is {name} and I am {age}')
 
 # String Methods
+s = 'hello world'
+
+#Capitalize string
+print(s.capitalize())
